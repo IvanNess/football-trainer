b03849848a13b70f40908ac9aaea14158d3698e2
IvanNess <chai@bk.ru>
2020-07-29 00:32:42 +0300

    add user results to server during logging when ?record in the url

--- src/log-in/log-in.js
+++ src/log-in/log-in.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import React, { useEffect, useState, useCallback } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import { Form, Input, Button, message } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
@@ -10,34 +10,72 @@ import PlayButton from '../play-btn'
 
 import './log-in.scss'
 
-const LogIn = ({user, setUsername}) => {
-    const [{error, isLoading, data}, doFetch] = useFetch(`${process.env.REACT_APP_SERVER_URI}/login`)
+const LogIn = ({ user, setUsername, score, questionNumber }) => {
+    const [{ error, isLoading, data }, doFetch] = useFetch(`${process.env.REACT_APP_SERVER_URI}/login`)
 
-    useEffect(()=>{
-        if(error && error.response.status===403){
+    const [{ isLoading: recordIsLoading, data: recordData }, recordDoFetch] = useFetch(`${process.env.REACT_APP_SERVER_URI}/record`)
+
+    const [isRecord, setIsRecord] = useState(false)
+
+    useEffect(() => {
+        setIsRecord(document.location.search === '?record')
+    }, [])
+
+    useEffect(() => {
+        if (error && error.response.status === 403) {
             message.error('Wrong user or password')
         }
     }, [error])
 
-    if(user.isLoaded && user.username)
-        return <Redirect to={`/user/${user.username}`}/>
+    const recordDoFetchCb = useCallback(recordDoFetch, [])
+
+    useEffect(()=>{
+        if (user.isLoaded && user.username && isRecord && !recordData) {
+            console.log('recordDoFetchCb', isRecord, user, recordData, questionNumber, score)
+            recordDoFetchCb({
+                data: {
+                    score, questionNumber
+                }
+            }) 
+        }
+    }, [ recordDoFetchCb, user, recordData, isRecord, questionNumber, score])
+ 
+
+    useEffect(()=>{
+        if(recordData){
+            console.log('recordData', recordData)
+            setIsRecord(false)
+        }
+    }, [recordData, setIsRecord])
+
+    useEffect(()=>{
+        console.log('data', data)
+        if (data && data.username) {
+            setUsername({ username: data.username })
+        }
+    }, [data, setUsername])
+
+
+    if (user.isLoaded && user.username && !isRecord) {
+        return <Redirect to={`/user/${user.username}`} />
+    }
+
+
 
     const onFinish = async values => {
         console.log('Received values of form: ', values)
-        doFetch({data: {
-            username: values.username,
-            password: values.password,
-        }})
+        doFetch({
+            data: {
+                username: values.username,
+                password: values.password,
+            }
+        })
     }
 
-    console.log('data', data)
-    if(data && data.username){
-        setUsername({username: data.username})
-    }
 
     return (
         <div className={`login-page`}>
-            <UserLoader/>
+            <UserLoader />
             <div className={`wrapper`}>
                 <h3>Authorization Page</h3>
                 <Form
@@ -77,25 +115,25 @@ const LogIn = ({user, setUsername}) => {
                     </Form.Item>
 
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button" disabled={isLoading}>
+                        <Button type="primary" htmlType="submit" className="login-form-button" disabled={isLoading || recordIsLoading}>
                             Log in
                         </Button>
-                        <a href="/signup">...or register now!</a> 
+                        <a href="/signup">...or register now!</a>
                     </Form.Item>
                 </Form>
                 ...or skip authorization and proceed to the game
-                <PlayButton/>
+                <PlayButton />
             </div>
         </div>
     )
 }
 
-const mapStateToProps = ({user})=>({
-    user
+const mapStateToProps = ({ user, score, questionNumber }) => ({
+    user, score, questionNumber
 })
 
 const mapDispatchToProps = ({
-    setUsername: (payload)=>({type: 'SET_USERNAME', payload})
+    setUsername: (payload) => ({ type: 'SET_USERNAME', payload })
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogIn)
