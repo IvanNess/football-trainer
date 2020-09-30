dc1dc7d63f0a3d034e0b8fe3210dd68565e0ae28
IvanNess <chai@bk.ru>
2020-09-30 20:27:19 +0300

    Updates

---
+++ precache-manifest.6a4f43c28441abd91c760b22abf125e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0212e70055867330bf747fa78a6c38f",
+    "url": "/football-years/index.html"
+  },
+  {
+    "revision": "76b109a471e1e7f29079",
+    "url": "/football-years/static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "6df4a46407834eb6ab31",
+    "url": "/football-years/static/css/main.afe33495.chunk.css"
+  },
+  {
+    "revision": "76b109a471e1e7f29079",
+    "url": "/football-years/static/js/2.69770245.chunk.js"
+  },
+  {
+    "revision": "ef20b9ddd7cf7cf5039dc421fe655af9",
+    "url": "/football-years/static/js/2.69770245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df4a46407834eb6ab31",
+    "url": "/football-years/static/js/main.cd774e76.chunk.js"
+  },
+  {
+    "revision": "c96c92b8bdcd0b566b87",
+    "url": "/football-years/static/js/runtime-main.246fe17c.js"
+  },
+  {
+    "revision": "0bd56bcc7f86de9bdb8baf6b14e6d34d",
+    "url": "/football-years/static/media/blackburn.0bd56bcc.png"
+  }
+]);
